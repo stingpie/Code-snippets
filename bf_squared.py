106e912dc155093c98f945242e2d4e64406e17dd
stingpie <53981910+stingpie@users.noreply.github.com>
2020-07-10 11:42:53 -0500

    Update bf_squared.py

--- bf_squared.py
+++ bf_squared.py
@@ -26,31 +26,26 @@ program=["",
          # and then puts them into an array, and sets that as it's own
          # variables list. 
          "self.variables=[] \nfor i in range(len(self.linked)): \n   self.variables=numpy.append(self.variables,actorlist[self.linked[i]].variables)",
-
-         # tiles 13-25 give orders to all the linked actors
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=0",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=1",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=2",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=3",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=4",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=5",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=6",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=7",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=8",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=9",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=10",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=11",
-         "for i in range(len(self.linked)): \n actorlist[self.linked[i]].orders=12",
-
-
-
+         #these next few edit the layer. 
+         "matrixdatabase[self.pos]+=1",
+         "matrixdatabase[self.pos]-=1",
+         "matrixdatabase[self.pos]+=self.variables[0]",
+         "matrixdatabase[self.pos]-=self.variables[0]",
+         #makes the actor active/inactive
+         "self.active = not self.active",
+         #adds an actor with the index variable[0]
+         "self.linked=numpy.append(self.linked,self.variables[0])",
+         #Removes the linked actors of index variables[0]
+         "self.linked=numpy.append(self.linked[:self.variables[0]],self.linked[self.variables[0]+1:])", 
+         #this creates a new actor using six variables from self.variables[0:5]
+         "actorlist=numpy.append(actorlist,actor(self.variables[0:2],self.variables[3:4],bool(self.variables[5])))",
 
          ] 
 
 for i in range(layercount): #this part appends arrays of matrixshape 
     matrixdatabase=numpy.append(matrixdatabase,numpy.zeros(matrixshape, dtype=int),axis=0)
 
-def pos_if(variable1, pos1):
+def pos_if(variable1, pos1): 
     # this checks all actors and if they are close to pos1,
     # it checks if any of that actor's variables equal variable1.
 
@@ -95,17 +90,31 @@ class actor(object):
         if self.active:
             # executes the code from the specific tile that the actor
             # stands on
-            exec(program[int(matrixdatabase[self.returnpos()])])
-
+            tile=int(matrixdatabase[self.returnpos()]) 
+            if tile <= len(program):
+                exec(program[tile])
+##                print(program[tile])
+##                print()
+            else:
+                # if the tile has a value larger than the length of "program"
+                # Assume it's trying to give an order to it's linked partners.
+                # len(program) * which_linked_partner + tile_order
+                linked_actor=int(tile/len(program))-1
+                linked_order=tile%len(program)
+                actorlist[self.linked[linked_actor]].orders=linked_order
+##                print(program[linked_order])
+##                print()
+                            
         else:
             #print(program[self.orders])
             exec(program[self.orders])
-
+##            print(program[self.orders])
+##            print()
             self.orders=0
         # the actor slides if there is no code against it. 
         self.pos[1]+=self.dir[0]
         self.pos[2]+=self.dir[1]
-
+        
 
 # This makes a fun path.
 # tile zero does nothing,
@@ -115,25 +124,24 @@ class actor(object):
 # tile 10 is the "if equal" tile
 # tile eleven gets the actor to read what tile the actor is standing on
 # tile twelve accumulates the data from it's linked partners
-# the tiles 13-26 give orders to the actor's linked partners Ex:(tile 13 tells
-# the partner to act as if it's on tile 0) 
+
 matrixdatabase[0,0,0]=1
-matrixdatabase[0,1,0]=14
-matrixdatabase[0,3,0]=24
+matrixdatabase[0,1,0]=22
+matrixdatabase[0,3,0]=32
 matrixdatabase[1,2,0]=12
 matrixdatabase[0,4,0]=12
 matrixdatabase[0,5,0]=10
-matrixdatabase[0,7,2]=18
+matrixdatabase[0,7,2]=27
 # So this program is set up to use 3 actors.
 # the main actor starts at [0,0,0], it's linked counterpart starts at [1,0,0]
 # the last actor is at [0,5,1] to act as an argument later.
 #
 # The program starts with [0,0,0] and tile 1, telling actor 0 to move to the
 # right.
-# Then, at [0,1,0], there is tile 14, telling the actor to command it's
+# Then, at [0,1,0], there is tile 23, telling the actor to command it's
 # linked counterpart to move right as well.
 # Then, as actor 1 (actor 0's counterpart) walks over tile 12, actor 0 goes
-# over tile 24, which tell's it's counterpart to read it's tile. Thus,
+# over tile 32, which tell's it's counterpart to read it's tile. Thus,
 # actor 1 stores tile 12 as it's variable.
 # Then, actor 0 walks over tile twelve, which tells it to take the variable
 # from it's counterpart.
@@ -159,7 +167,7 @@ c=actor([0,5,1],[0,0],False)
 c.variables=12
 
 actorlist=[a,b,c]
-print(program[24])
+
 for i in range(8):
     print("iter "+str(i)+":")
     for i in range(len(actorlist)):
